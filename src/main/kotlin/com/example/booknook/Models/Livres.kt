fa0285d8e62280b7dbf84bfcbcea39f3fa80018a
Manaf Soulaimane <smanaf@ordralphabetix>
2024-10-23 17:32:34 -0400

    Ajout des classes Livres et Utilisateurs

---
+++ src/main/kotlin/com/example/booknook/Models/Livres.kt
@@ -0,0 +1,7 @@
+package com.example.booknook
+
+data class Livres( val isbn: Int, 
+                  val nom: String,
+                  val auteur: String,
+                  val quantite: Int,
+                  )
---
+++ src/main/kotlin/com/example/booknook/Models/Utilisateurs.kt
@@ -0,0 +1,7 @@
+package com.example.booknook
+
+data class Utilisateurs(val id: Int, 
+                  val nom: String,
+                  val auteur: String,
+                  val type: Boolean,
+                  )
